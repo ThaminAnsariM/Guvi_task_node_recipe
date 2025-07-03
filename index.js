9a17b2e4b40dde4030d5ec8f12cbeec9fdb972bd
ThaminAnsariM <thameenansari456@gmail.com>
2025-07-03 22:56:26 +0530

    port

--- index.js
+++ index.js
@@ -141,6 +141,6 @@ app.delete("/deleteRecipe", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
